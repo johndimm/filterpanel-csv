12a485561cb64f64092e67756ab0170d4e8e95a9
John Dimm <john.dimm@galigo.com>
2021-08-02 17:39:54 -0700

    Revisions to intro text.

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,6 +48,142 @@ return <pre>
 </pre>
 }
 
+const Movies = () => {
+	return 		( 	
+	<div>
+		<h3>Movies</h3>
+		<a href='/FetchCSV?url=https%3A%2F%2Fquery.data.world%2Fs%2Flt76nsufm5v76w2au4mmkksoh6wu6a'>
+			<img src='movies_thumbnail.png' />
+		</a>
+
+		<div>
+			<ul>
+			<li>CSV file from <a href='https://data.world/studentoflife/imdb-top-250-lists-and-5000-or-so-data-records'>data.world</a>
+			</li>
+			
+			<li>
+			Uses extracts from IMDB and OMDB, curated by TheMitchWorksPro. 
+			</li>
+			</ul>
+		</div>
+	</div>)
+
+}
+
+const Shapes = () => {
+	return (
+		<div>
+			<h3>Shapes</h3>
+			<a href='https://filterpanel-example.vercel.app/'><img src='shapes.png'/></a>
+			<ul>
+				<li>
+					Trivial example using a JSON literal as input.
+				</li>
+				<li>
+					Click a few checkboxes and watch how the "in" and "out" masks change.
+				</li>
+			</ul>
+		</div>
+	)
+}
+
+const OlympicMedals = () => {
+	return (
+				<div>
+					<h3>Olympic Medals</h3>
+					<a href='/FetchCSV?url=https%3A%2F%2Fquery.data.world%2Fs%2F45jwacgkggx55n2p3yox4qyiug7n6l'>
+						<img src='olympics_thumbnail.png' />
+					</a>
+					<ul>
+						<li>CSV file from <a href='https://data.world/johndimm/olympic-medals-1896-to-2014-in-detail'>data.world</a></li>
+						<li>uses data released by <a href='https://www.theguardian.com/sport/datablog/2012/jun/25/olympic-medal-winner-list-data#data'>The Guardian</a></li>
+						
+						 <li>hosted on <a href='https://www.kaggle.com/the-guardian/olympic-games'>Kaggle</a> and many other sites</li>
+						 <li>
+						 covers Olympic medals up to 2014
+							 </li>
+					</ul>
+				</div>		
+	)
+}
+
+const PublicPrivate = () => {
+	return (
+		<div>
+			<h2>Public vs Private</h2>
+			<p>
+				When you open a file on your computer, the data will be read into Filterpanel running on
+				your browser but will not be transmitted over the internet.
+			</p>
+			<p>
+				There is no Filterpanel server. There are no accounts. There is no database. Nothing is
+				stored. It's just a script.  The entire CSV file is read on startup every time, either from local storage or the internet.  The second time is faster, because caching.
+			</p>
+			<p>
+				To share a local CSV file as rendered by Filterpanel, you need to make it available on the
+				internet using a url. One option is to upload it to data.world.  Another is to send the file to your contacts and ask them to load it into Filterpanel.
+			</p>
+		</div>
+	)
+}
+
+const GetLocalFile = () => {
+	return (
+		<div className={styles.public_data}>
+			<h2>Public Data</h2>
+			<div className={styles.instructions}>Enter the url of a CSV file.</div>
+			<form action='/FetchCSV'>
+				<span>url:</span>
+				<input type='text' name='url'></input>
+				<button type='submit'>fetch</button>
+			</form>
+		</div>
+	)
+}
+
+const GetNetworkFile = () => {
+	return (
+		<div className={styles.private_data}>
+			<h2>Private Data</h2>
+			<div className={styles.instructions}>Open a CSV file on your computer.</div>
+			<form id='fileForm' encType='multipart/form-data' method='post'>
+				<input
+					id='files'
+					type='file'
+					onChange={(e) => {
+						const f = document.getElementById('fileForm')
+						uploadFile(f.elements['files'].files[0])
+					}}
+				/>
+			</form>
+		</div>
+	)
+}
+
+const GitHub = () => {
+	return (
+		<div>
+			<h2 >Github</h2>
+			<h3>filterpanel</h3>
+            <a href='https://github.com/johndimm/filterpanel'>https://github.com/johndimm/filterpanel</a>
+			<div style={{marginTop: "10px"}}>
+				The node module exposes the Filterpanel component.  It takes a JSON array as input.
+			</div>
+			<br />
+			<h3>filterpanel-example</h3>
+			<a href='https://github.com/johndimm/filterpanel-example'>https://github.com/johndimm/filterpanel-example</a>
+            <div style={{marginTop: "10px"}}>
+				A toy example showing how it works.  Data is a JSON literal.
+			</div>
+			<h3>filterpanel-csv</h3>
+            <a href='https://github.com/johndimm/filterpanel-csv'>https://github.com/johndimm/filterpanel-csv</a>
+			<div style={{marginTop: "10px"}}>
+				This app takes input from CSV files, either from the local system or the internet.  It scans and analyzes the CSV and applies rules to layout the filterpanel.
+			</div>
+		</div>
+	)
+}
+
 function MainPage() {
 	const [csvBuffer, setCsvBuffer] = useState([])
 
@@ -76,95 +212,26 @@ function MainPage() {
 				automatically generate a filter panel user interface using only a CSV file
 			</div>
 
-			<div>
-				<div className={styles.public_data}>
-					<h2>Public Data</h2>
-					<div className={styles.instructions}>Enter the url of a CSV file.</div>
-					<form action='/FetchCSV'>
-						<span>url:</span>
-						<input type='text' name='url'></input>
-						<button type='submit'>fetch</button>
-					</form>
-				</div>
-
-				<div className={styles.private_data}>
-					<h2>Private Data</h2>
-					<div className={styles.instructions}>Open a CSV file on your computer.</div>
-					<form id='fileForm' encType='multipart/form-data' method='post'>
-						<input
-							id='files'
-							type='file'
-							onChange={(e) => {
-								const f = document.getElementById('fileForm')
-								uploadFile(f.elements['files'].files[0])
-							}}
-						/>
-					</form>
-				</div>
-			</div>
+            <GetLocalFile />
+			<GetNetworkFile />
 
 			<div className={styles.article_text}>
-				<h2>Public vs Private</h2>
-				<p>
-					When you open a file on your computer, the data will be read into Filterpanel running on
-					your browser but will not be transmitted over the internet.
-				</p>
-				<p>
-					There is no Filterpanel server. There are no accounts. There is no database. Nothing is
-					stored. It's just a script.  The entire CSV file is read on startup every time, either from local storage or the internet.  The second time is faster, because caching.
-				</p>
-				<p>
-					To share a local CSV file as rendered by Filterpanel, you need to make it available on the
-					internet using a url. One option is to upload it to data.world.  Another is to send the file to your contacts and ask them to load it into Filterpanel.
-				</p>
 
-				<h2>Public Data Examples</h2>
-				<div>
-					<h3>Movies</h3>
-					<a href='/FetchCSV?url=https%3A%2F%2Fquery.data.world%2Fs%2Flt76nsufm5v76w2au4mmkksoh6wu6a'>
-						<img src='movies_thumbnail.png' />
-					</a>
+				<h2>Examples</h2>
+				<Shapes />
+				<Movies />
+				<OlympicMedals />
 
-					<div>
-						<ul>
-						<li>CSV file from <a href='https://data.world/studentoflife/imdb-top-250-lists-and-5000-or-so-data-records'>data.world</a>
-						</li>
-						
-			            <li>
-						Uses extracts from IMDB and OMDB, curated by TheMitchWorksPro. 
-						</li>
-						</ul>
-					</div>
-				</div>
+				<h2>Sample records</h2>
+				<h3>Movie</h3>
+				<SampleMovie />
+				<h3>Olympic Medal</h3>
+				<SampleOlympic />
 
-				<div>
-					<h3>Olympic Medals</h3>
-					<a href='/FetchCSV?url=https%3A%2F%2Fquery.data.world%2Fs%2F45jwacgkggx55n2p3yox4qyiug7n6l'>
-						<img src='olympics_thumbnail.png' />
-					</a>
-					<ul>
-						<li>CSV file from <a href='https://data.world/johndimm/olympic-medals-1896-to-2014-in-detail'>data.world</a></li>
-						<li>uses data released by <a href='https://www.theguardian.com/sport/datablog/2012/jun/25/olympic-medal-winner-list-data#data'>The Guardian</a></li>
-						
-						 <li>hosted on <a href='https://www.kaggle.com/the-guardian/olympic-games'>Kaggle</a> and many other sites</li>
-						 <li>
-						 covers Olympic medals up to 2014
-							 </li>
-					</ul>
-				</div>
-			<h2>Sample records</h2>
-			<h3>Movie</h3>
-			<SampleMovie />
-			<h3>Olympic Medal</h3>
-			<SampleOlympic />
+				<PublicPrivate />
+				<GitHub />
 
 
-			<h2 >Github</h2>
-			<div>
-				Filterpanel: <a href='https://github.com/johndimm/filterpanel'>https://github.com/johndimm/filterpanel</a> 
-			<br />
-			    Filterpanel-csv: <a href='https://github.com/johndimm/filterpanel-csv'>https://github.com/johndimm/filterpanel-csv</a>
-			</div>
 			</div>
 		</div>
 	)
